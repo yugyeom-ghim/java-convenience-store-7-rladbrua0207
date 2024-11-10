057fa52f9cd289c725d1649abba7fae939d9c850
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-10 21:15:11 +0900

    feat: 프로모션 추가 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,14 +7,19 @@ import org.graalvm.collections.Pair;
 
 public class InputView {
 
+    private static final String ERROR_FORMAT_MESSAGE = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+    private static final String ERROR_INVALID_INPUT_MESSAGE = "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.";
     private static final String INPUT_PURCHASE_PRODUCTS_AND_QUANTITIES_MESSAGE =
             "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
-    private static final String ERROR_FORMAT_MESSAGE = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+    private static final String INPUT_RECEIVE_INTENT_ONE_MORE_FREE_MESSAGE =
+            "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_HYPHEN = "-";
+    private static final String YES = "Y";
+    private static final String NO = "N";
     private static final char OPEN_BRACKET = '[';
     private static final char CLOSE_BRACKET = ']';
-    private static final String DELIMITER_HYPHEN = "-";
     private static final int PRODUCT_INDEX = 0;
     private static final int QUANTITY_INDEX = 1;
 
@@ -23,6 +28,18 @@ public class InputView {
         String input = Console.readLine();
         validatePurchaseFormat(input);
         return parsePurchaseInput(input);
+    }
+
+    public static boolean inputReceiveIntentOneMoreFree(String productName) {
+        System.out.println(INPUT_RECEIVE_INTENT_ONE_MORE_FREE_MESSAGE);
+        String input = Console.readLine();
+        validateYerOrNo(input);
+        return input.equals(YES);
+    }
+    private static void validateYerOrNo(String input) {
+        if (!(input.equals(YES) || input.equals(NO))) {
+            throw new IllegalArgumentException(ERROR_INVALID_INPUT_MESSAGE);
+        }
     }
 
     private static void validatePurchaseFormat(String input) {
