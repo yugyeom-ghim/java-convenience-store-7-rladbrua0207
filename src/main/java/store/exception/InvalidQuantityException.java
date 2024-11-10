3c3a0828d74962ee0760614d96d5943a16f40bad
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-10 15:51:41 +0900

    feat: 커스텀 exception 추가

---
+++ src/main/java/store/exception/InvalidQuantityException.java
@@ -0,0 +1,8 @@
+package store.exception;
+
+public class InvalidQuantityException extends RuntimeException{
+
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
+}
