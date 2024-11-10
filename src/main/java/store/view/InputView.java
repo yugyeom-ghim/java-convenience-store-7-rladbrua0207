1c9fe29b7fb8c0c19be795bdef55b92d913edb92
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-10 21:16:23 +0900

    feat: 프로모션 재고 부족 시 정가로 결제할지 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -13,6 +13,8 @@ public class InputView {
             "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String INPUT_RECEIVE_INTENT_ONE_MORE_FREE_MESSAGE =
             "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+    private static final String INPUT_PURCHASE_INTENT_NOT_APPLY_PROMOTION_MESSAGE =
+            "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
 
     private static final String DELIMITER_COMMA = ",";
     private static final String DELIMITER_HYPHEN = "-";
@@ -36,6 +38,14 @@ public class InputView {
         validateYerOrNo(input);
         return input.equals(YES);
     }
+
+    public static boolean inputPurchaseIntentNotApplyPromotion(String productName, int quantity) {
+        System.out.printf((INPUT_PURCHASE_INTENT_NOT_APPLY_PROMOTION_MESSAGE) + "\n", productName, quantity);
+        String input = Console.readLine();
+        validateYerOrNo(input);
+        return input.equals(YES);
+    }
+
     private static void validateYerOrNo(String input) {
         if (!(input.equals(YES) || input.equals(NO))) {
             throw new IllegalArgumentException(ERROR_INVALID_INPUT_MESSAGE);
