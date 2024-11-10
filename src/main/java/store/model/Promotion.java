dadab24c7821f61ffd892495c790b201935b5921
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-10 17:18:51 +0900

    feat: 프로모션 날짜 검증 구현

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,20 +1,28 @@
 package store.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Promotion {
 
+    private static final int ONE = 1;
+
     private String name;
     private int buyCount;
-    private int getCount;
+    private int freeCount;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Promotion(String name, int buyCount, int getCount, LocalDate startDate, LocalDate endDate) {
+    public Promotion(String name, int buyCount, int freeCount, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.buyCount = buyCount;
-        this.getCount = getCount;
+        this.freeCount = freeCount;
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public Boolean isValidDate(LocalDateTime now) {
+        return (startDate.atStartOfDay().isBefore(now) || startDate.atStartOfDay().isEqual(now))
+                && endDate.plusDays(ONE).atStartOfDay().isAfter(now);
     }
 }
