d3a1eae43b7147d7a23407cde8dba9120db0496e
YugyeomGhim <rladbrua0207@gmail.com>
2024-11-11 16:46:08 +0900

    test: 잘못된 수량 테스트 수정

--- src/test/java/store/model/OrderTest.java
+++ src/test/java/store/model/OrderTest.java
@@ -3,7 +3,6 @@ package store.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import store.exception.NotFoundException;
 
 class OrderTest {
 
@@ -11,11 +10,11 @@ class OrderTest {
     public void 수량이_0_이하일_경우_예외() throws Exception {
         // given
         String productName = "콜라";
-        int quantity = 6;
+        int quantity = 0;
 
         // when & then
         Assertions.assertThatThrownBy(() -> new Order(productName, quantity))
-                .isInstanceOf(NotFoundException.class);
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
